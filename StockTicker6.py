b08dbfc467bb828a0d1b3c3bbec4ce17f37249ea
aibr4ever <abir4ever@gmail.com>
2020-11-12 17:56:34 -0800

    Initial launch

---
+++ StockTicker6.py
@@ -0,0 +1,98 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import pandas_datareader.data as web
+from datetime import datetime
+import os
+import pandas as pd
+import yfinance as yf
+import dash_auth
+
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'hello': 'world'
+}
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
+# app = dash.Dash()
+# auth = da.basic_auth(app,USERNAME_PASSWORD_PAIRS)
+# read a .csv file, make a dataframe, and build a list of Dropdown options
+nsdq = pd.read_csv('../data/NASDAQcompanylist.csv')
+nsdq.set_index('Symbol', inplace=True)
+options = []
+for tic in nsdq.index:
+    options.append({'label':'{} {}'.format(tic,nsdq.loc[tic]['Name']), 'value':tic})
+
+app.layout = html.Div([
+    html.H1('Stock Ticker Dashboard'),
+    html.Div([
+        html.H3('Select stock symbols:', style={'paddingRight':'30px'}),
+        # replace dcc.Input with dcc.Options, set options=options
+        dcc.Dropdown(
+            id='my_ticker_symbol',
+            options=options,
+            value=['TSLA'],
+            multi=True
+        )
+    # widen the Div to fit multiple inputs
+    ], style={'display':'inline-block', 'verticalAlign':'top', 'width':'30%'}),
+    html.Div([
+        html.H3('Select start and end dates:'),
+        dcc.DatePickerRange(
+            id='my_date_picker',
+            min_date_allowed=datetime(2015, 1, 1),
+            max_date_allowed=datetime.today(),
+            start_date=datetime(2018, 1, 1),
+            end_date=datetime.today()
+        )
+    ], style={'display':'inline-block'}),
+    html.Div([
+        html.Button(
+            id='submit-button',
+            n_clicks=0,
+            children='Submit',
+            style={'fontSize':24, 'marginLeft':'30px'}
+        ),
+    ], style={'display':'inline-block'}),
+    dcc.Graph(
+        id='my_graph',
+        figure={
+            'data': [
+                {'x': [1,2], 'y': [3,1]}
+            ]
+        }
+    )
+])
+@app.callback(
+    Output('my_graph', 'figure'),
+    [Input('submit-button', 'n_clicks')],
+    [State('my_ticker_symbol', 'value'),
+    State('my_date_picker', 'start_date'),
+    State('my_date_picker', 'end_date')])
+def update_graph(n_clicks, stock_ticker, start_date, end_date):
+    start = datetime.strptime(start_date[:10], '%Y-%m-%d')
+    end = datetime.strptime(end_date[:10], '%Y-%m-%d')
+    # since stock_ticker is now a list of symbols, create a list of traces
+    traces = []
+    for tic in stock_ticker:
+        # df = web.DataReader(tic,'iex',start,end)
+        df = yf.download(tic, start=start, end=end)
+        traces.append({'x':df.index, 'y': df['Adj Close'], 'name':tic})
+    fig = {
+        # set data equal to traces
+        'data': traces,
+        # use string formatting to include all symbols in the chart title
+        'layout': {'title':', '.join(stock_ticker)+' Closing Prices'}
+    }
+    return fig
+
+if __name__ == '__main__':
+    os.environ["IEX_API_KEY"] = 'pk_3b40de02cd9f469dbffc87f8c114e13d'
+    app.run_server()
